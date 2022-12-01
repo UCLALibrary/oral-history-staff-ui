875cef2f25eea9f43c3fd4b1bd0d03ce72462c14
Kristian Allen <kallen2@library.ucla.edu>
2022-12-01 15:10:30 -0800

    Override of default plural table name for ItemStatus

--- oh_staff_ui/models.py
+++ oh_staff_ui/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class ItemStatus(models.Model):
     status = models.CharField(max_length=40)
     status_description = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name_plural = "Item statuses"
