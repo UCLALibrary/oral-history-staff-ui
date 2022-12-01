e3eba7c17ba682334a49284f7911ab706710c999
Kristian Allen <kallen2@library.ucla.edu>
2022-12-01 12:50:52 -0800

    Added ItemStatus model and migration

--- oh_staff_ui/models.py
+++ oh_staff_ui/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ItemStatus(models.Model):
+    status = models.CharField(max_length=40)
+    status_description = models.CharField(max_length=256)
