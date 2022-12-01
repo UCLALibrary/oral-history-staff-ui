3a544965ee5416e165ed7b92f01b48a6c06b032b
Kristian Allen <kallen2@library.ucla.edu>
2022-12-01 14:47:50 -0800

    Addition of ItemStatus model to admin interface

--- oh_staff_ui/admin.py
+++ oh_staff_ui/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ItemStatus
 
-# Register your models here.
+
+@admin.register(ItemStatus)
+class ItemStatus(admin.ModelAdmin):
+    list_display = ("status", "status_description")
