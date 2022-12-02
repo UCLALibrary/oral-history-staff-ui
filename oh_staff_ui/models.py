68321d8ab13091aa79d23c0e167dcff5d1956dc8
Kristian <kjallen@users.noreply.github.com>
2022-12-01 16:44:02 -0800

    SYS-1095: Add Item Status model (#2)

--- oh_staff_ui/models.py
+++ oh_staff_ui/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ItemStatus(models.Model):
+    status = models.CharField(max_length=40)
+    status_description = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name_plural = "Item statuses"
