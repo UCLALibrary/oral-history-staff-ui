2256ae7b2a9b9ee10416810cfd62a536762e88a8
Kristian Allen <kallen2@library.ucla.edu>
2022-12-07 17:28:10 -0800

    Initial add of ProjectItem model outline

--- oh_staff_ui/models.py
+++ oh_staff_ui/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,3 +18,20 @@ class ItemType(models.Model):
 
     def __str__(self):
         return self.type
+
+
+class ProjectItem(models.Model):
+
+    ark = models.CharField(max_length=40, blank=False, null=False)
+    create_date = models.DateField()
+    created_by = models.ForeignKey(User)
+    last_edit_date = models.DateField()
+    last_modified_by = models.ForeignKey(User)
+    parent = models.ForeignKey("self")
+    sequence = models.IntegerField()
+    status = models.ForeignKey(ItemStatus, blank=True, null=True)
+    title = models.CharField(max_length=256, blank=False, null=False)
+    type = models.ForeignKey(ItemType, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
