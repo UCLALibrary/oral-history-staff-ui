574d068552914a3c35ba52742028661ff2214e39
Andy Kohler <akohler@library.ucla.edu>
2023-03-01 19:36:41 -0900

    Add extremely minimal view and template for showing item data

--- oh_staff_ui/urls.py
+++ oh_staff_ui/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("add_item/", views.add_item),
+    path("add_item/", views.add_item, name="add_item"),
+    path("item/<int:item_id>", views.edit_item, name="edit_item"),
     path("", views.add_item),  # TODO: Change this to something better.....
 ]
--- oh_staff_ui/views.py
+++ oh_staff_ui/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.http.request import HttpRequest  # for code completion
+from django.http.response import HttpResponse  # for code completion
 from oh_staff_ui.forms import ProjectItemForm
 from oh_staff_ui.models import ProjectItem
 
 
 @login_required
-def add_item(request):
+def add_item(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = ProjectItemForm(request.POST)
         if form.is_valid():
             # Minimal data from form;
             # most other fields set by db defaults or web service.
-            # sequence = form.cleaned_data["sequence"]
-            # title = form.cleaned_data["title"]
             # TODO: Get this from web service
             ark = "fake ark"
-            # TODO: Get real user info
+            # User must be logged in, so is present in request
             user = request.user
             new_item = ProjectItem(
                 ark=ark,
@@ -26,6 +26,16 @@ def add_item(request):
                 last_modified_by=user,
             )
             new_item.save()
+            # Pass newly created item to edit form
+            item_id = new_item.pk
+            return redirect("edit_item", item_id=item_id)
     else:
         form = ProjectItemForm()
-    return render(request, "oh_staff_ui/add_item.html", {"form": form})
+        return render(request, "oh_staff_ui/add_item.html", {"form": form})
+
+
+@login_required
+def edit_item(request: HttpRequest, item_id: int) -> HttpResponse:
+    # TODO: Real form, and view, for editing item and its associated metadata.
+    item = ProjectItem.objects.get(pk=item_id)
+    return render(request, "oh_staff_ui/edit_item.html", {"item": item})
