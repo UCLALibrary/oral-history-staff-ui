f79d48297c7e30278e890b182989461362e107b2
Zoe Tucker <zoetucker@library.ucla.edu>
2023-03-03 13:08:21 -0800

    add basic search for ARK and Title

--- oh_staff_ui/urls.py
+++ oh_staff_ui/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path("add_item/", views.add_item, name="add_item"),
     path("item/<int:item_id>", views.edit_item, name="edit_item"),
     path("", views.add_item),  # TODO: Change this to something better.....
+    path("item_search/", views.item_search, name="item_search"),
+    path(
+        "search_results/<str:search_type>/<str:query>",
+        views.search_results,
+        name="search_results",
+    ),
 ]
