78d108ac620f89ee339e99fb03d705ae44c607f2
Andy Kohler <akohler@library.ucla.edu>
2023-04-27 14:05:22 -0800

    Make search the default; add link for 'Add Item' to menu

--- oh_staff_ui/urls.py
+++ oh_staff_ui/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("add_item/", views.add_item, name="add_item"),
     path("add_item/<int:parent_id>", views.add_item, name="add_item_from_parent"),
     path("item/<int:item_id>", views.edit_item, name="edit_item"),
-    path("", views.add_item),  # TODO: Change this to something better.....
+    path("", views.item_search),
     path("item_search/", views.item_search, name="item_search"),
     path(
         "search_results/<str:search_type>/<path:query>",
