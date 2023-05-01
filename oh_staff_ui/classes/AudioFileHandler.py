c9eefb0024459f096b45786d047bcf08fee32beb
Andy Kohler <akohler@library.ucla.edu>
2023-05-01 15:31:58 -0800

    Delete temporary mp3 file after processing

--- oh_staff_ui/classes/AudioFileHandler.py
+++ oh_staff_ui/classes/AudioFileHandler.py
@@ -93,6 +93,10 @@ class AudioFileHandler(BaseFileHandler):
         except (ValueError, CommandError):
             # Re-raise back to caller
             raise
+        finally:
+            # Delete the temporary submaster file;
+            # don't throw FileNotFoundError if for some reason it doesn't exist.
+            Path(submaster_file_name).unlink(missing_ok=True)
 
     def _item_has_files(self) -> bool:
         """See if item associated with the media_file passed to this processor
