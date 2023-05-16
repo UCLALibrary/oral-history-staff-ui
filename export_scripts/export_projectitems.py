a9890793e705e52a2854514c709de59d66d60ead
Andy Kohler <akohler@library.ucla.edu>
2023-05-16 12:13:04 -0800

    SYS-1276: Migrate original DLCS user names as-is (#77)

--- export_scripts/export_projectitems.py
+++ export_scripts/export_projectitems.py
@@ -14,9 +14,9 @@ select
     a.item_ark as ark,
     c.coverage,
     a.create_date,
-    coalesce(uc.email, 'oralhistory@library.ucla.edu') as created_by,
+    a.created_by,
     a.last_edit_date as last_modified_date,
-    coalesce(um.email, 'oralhistory@library.ucla.edu') as last_modified_by,
+    a.modified_by as last_modified_by,
     b.item_ark as parent_ark,
     r.relation as relation,
     a.item_sequence as sequence,
@@ -27,19 +27,26 @@ from project_items a
     left outer join project_items b on a.parent_divid = b.divid_pk
     left outer join qa_status qa on a.statusid_fk = qa.statusid_pk
     left outer join dl_objects o on a.objectid_fk = o.objectid_pk
-    left outer join users uc on a.created_by = uc.user_name
-    left outer join users um on a.modified_by = um.user_name
-    left outer join (select listagg(desc_value, ',') as relation, divid_fk from desc_values
-where
-    projectid_fk = 80 and
-    desc_termid_fk = 899
-group by divid_fk) r on a.divid_pk = r.divid_fk
-left outer join (select listagg(desc_value, ',') as coverage, divid_fk from desc_values
-where
-    projectid_fk = 80 and
-    desc_termid_fk = 900
-group by divid_fk) c on a.divid_pk = c.divid_fk
-where a.projectid_fk = 80 order by create_date
+    left outer join (
+        select
+            listagg(desc_value, ',') as relation,
+            divid_fk from desc_values
+        where
+            projectid_fk = 80 and
+            desc_termid_fk = 899
+        group by divid_fk
+    ) r on a.divid_pk = r.divid_fk
+    left outer join (
+        select
+            listagg(desc_value, ',') as coverage,
+            divid_fk from desc_values
+        where
+            projectid_fk = 80 and
+            desc_termid_fk = 900
+        group by divid_fk
+    ) c on a.divid_pk = c.divid_fk
+where a.projectid_fk = 80
+order by create_date
 """
 
 con = oracledb.connect(user=api_keys.USER, password=api_keys.PASSWORD, dsn=api_keys.DSN)
