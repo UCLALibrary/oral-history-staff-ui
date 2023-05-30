02ae522516edecea5c5b8c01dc88af1aaa78fcfc
kjallen <kallen2@library.ucla.edu>
2023-05-30 16:12:17 -0700

    Removed unused imports, added DoesNotExist exception handling

--- oh_staff_ui/management/commands/create_mods_records.py
+++ oh_staff_ui/management/commands/create_mods_records.py
@@ -1,20 +1,20 @@
 import logging
 from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
 from oh_staff_ui.classes.OralHistoryMods import OralHistoryMods
 from oh_staff_ui.models import ProjectItem
-
-# For handling command-line processing
-from django.contrib.auth.models import User
-
 
 logger = logging.getLogger(__name__)
 
 
 def create_mods_records(item_id: int) -> None:
     """Given an item_id write the related MODS record to public location"""
-    pi = ProjectItem.objects.filter(id=item_id).first()
-    if pi:
+    try:
+        pi = ProjectItem.objects.get(id=item_id)
         OralHistoryMods(pi).write_mods_record()
+    except (ProjectItem.DoesNotExist) as e:
+        logger.error(e)
+        raise CommandError(f"ProjectItem with id {item_id} does not exist")
 
 
 class Command(BaseCommand):
