ff3304aa1c4c11103cbac7299caa00de22833ec2
Andy Kohler <akohler@library.ucla.edu>
2023-06-14 10:27:58 -0800

    Fix copy/paste error in file metadata loading command

--- docker_scripts/reset_prod_db.sh
+++ docker_scripts/reset_prod_db.sh
@@ -58,4 +58,4 @@ python manage.py import_names ${DATA_DIR}/Name.tsv
 python manage.py import_publishers ${DATA_DIR}/Publisher.tsv
 python manage.py import_resources ${DATA_DIR}/Type_of_Resource_.tsv
 python manage.py import_subjects ${DATA_DIR}/Subject.tsv
-python manage.py import_subjects ${DATA_DIR}/file_metadata.tsv
+python manage.py import_file_metadata ${DATA_DIR}/file_metadata.tsv
