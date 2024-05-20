ffcc2e920bdc06bb46a2ad929f147ff82cba7236
Andy Kohler <akohler@library.ucla.edu>
2024-05-20 15:19:18 -0800

    Add item_id to output, clean up comments and args

--- oh_staff_ui/management/commands/audit_media_files.py
+++ oh_staff_ui/management/commands/audit_media_files.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from django.core.management.base import BaseCommand, CommandParser
+from django.core.management.base import BaseCommand, CommandParser, CommandError
 from django.conf import settings
 from oh_staff_ui.models import MediaFile
 
@@ -15,7 +15,8 @@ class Command(BaseCommand):
         parser.add_argument(
             "mode",
             type=str,
-            help="Mode: one of DB|FILES|BOTH",
+            help="Mode: one of DB|FILES|ALL",
+            choices=["DB", "FILES", "ALL"],
         )
 
     def handle(self, *args, **options) -> None:
@@ -28,7 +29,7 @@ class Command(BaseCommand):
             self._compare_db_to_files()
             self._compare_files_to_db()
         else:
-            raise ValueError(f"Unknown mode: {mode}")
+            raise CommandError(f"Unknown mode: {mode}")
 
     def _compare_db_to_files(self) -> None:
         """Compares MediaFile data with files on disk and
@@ -39,7 +40,7 @@ class Command(BaseCommand):
             db_file_name = mf.file.name
             db_file_path = Path(settings.MEDIA_ROOT).joinpath(db_file_name)
             db_file_size = mf.file_size
-            # If file does exist, get info for comparison
+            # If file does exist, get info for comparison.
             if db_file_path.exists():
                 disk_file_size = db_file_path.stat().st_size
             else:
@@ -49,12 +50,12 @@ class Command(BaseCommand):
             if not db_file_path.exists():
                 logger.warning(
                     f"FILE MISSING:\t{db_file_path}\t{disk_file_path}\t"
-                    f"{db_file_size}\t{disk_file_size}"
+                    f"{db_file_size}\t{disk_file_size}\t{mf.item_id}"
                 )
             elif db_file_size != disk_file_size:
                 logger.warning(
                     f"SIZE DIFFERENCE:\t{db_file_path}\t{disk_file_path}\t"
-                    f"{db_file_size}\t{disk_file_size}"
+                    f"{db_file_size}\t{disk_file_size}\t{mf.item_id}"
                 )
 
     def _compare_files_to_db(self) -> None:
@@ -63,22 +64,24 @@ class Command(BaseCommand):
         """
         # Build this once, as it will be used many times.
         media_root = settings.MEDIA_ROOT + "/"
-        # rglob returns directory and file names.
         for path in Path(media_root).rglob("*"):
+            # rglob returns directory and file names; we only want files.
             if path.is_file():
-                # MediaFile file.name does not have MEDIA_ROOT prefix, so remove it.
+                # MediaFile file.name does not have MEDIA_ROOT prefix,
+                # so remove it from disk file name.
                 disk_file_path = str(path).replace(media_root, "")
                 disk_file_size = path.stat().st_size
                 media_files = MediaFile.objects.filter(file=disk_file_path)
                 media_file_count = len(media_files)
 
                 if media_file_count == 0:
-                    # File not found in database
+                    # File not found in database.
                     db_file_size = None
                     db_file_path = "NOT FOUND"
+                    item_id = None
                     logger.warning(
                         f"DB MISSING:\t{db_file_path}\t{disk_file_path}\t"
-                        f"{db_file_size}\t{disk_file_size}"
+                        f"{db_file_size}\t{disk_file_size}\t{item_id}"
                     )
                 elif media_file_count > 1:
                     # Shouldn't happen due to custom MediaFile.save()...
@@ -87,5 +90,5 @@ class Command(BaseCommand):
                     for mf in media_files:
                         logger.warning(
                             f"MULTIPLE:\t{mf.file.name}\t{disk_file_path}\t"
-                            f"{mf.file_size}\t{disk_file_size}"
+                            f"{mf.file_size}\t{disk_file_size}\t{mf.item_id}"
                         )
