2c2e07bc10bcd40af68320527ecb4a684f47340a
Zoe Tucker <zoetucker@library.ucla.edu>
2025-05-16 12:30:44 -0700

    SYS-1829: Update Django, Python, and PostgreSQL (#140)
    * update Python, Django, and PostgreSQL
    * fix logout link

--- docker_scripts/entrypoint.sh
+++ docker_scripts/entrypoint.sh
@@ -11,7 +11,7 @@ fi
 
 # Check when database is ready for connections
 echo "Checking database connectivity..."
-until python -c 'import os, psycopg2 ; conn = psycopg2.connect(host=os.environ.get("DJANGO_DB_HOST"),port=os.environ.get("DJANGO_DB_PORT"),user=os.environ.get("DJANGO_DB_USER"),password=os.environ.get("DJANGO_DB_PASSWORD"),dbname=os.environ.get("DJANGO_DB_NAME"))' ; do
+until python -c 'import os, psycopg ; conn = psycopg.connect(host=os.environ.get("DJANGO_DB_HOST"),port=os.environ.get("DJANGO_DB_PORT"),user=os.environ.get("DJANGO_DB_USER"),password=os.environ.get("DJANGO_DB_PASSWORD"),dbname=os.environ.get("DJANGO_DB_NAME"))' ; do
   echo "Database connection not ready - waiting"
   sleep 5
 done
