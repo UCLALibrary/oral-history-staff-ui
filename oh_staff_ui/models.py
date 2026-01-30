ecd307bdeed159430a590e900d138a298e7da54a
Andy Kohler <akohler@library.ucla.edu>
2026-01-29 15:26:31 -0900

    Change Description/Item FK relation from PROTECT to CASCADE

--- oh_staff_ui/models.py
+++ oh_staff_ui/models.py
@@ -290,7 +290,7 @@ class DescriptionType(models.Model):
 class Description(models.Model):
     value = models.CharField(max_length=8000, blank=False, null=False)
     item = models.ForeignKey(
-        ProjectItem, on_delete=models.PROTECT, blank=False, null=False
+        ProjectItem, on_delete=models.CASCADE, blank=False, null=False
     )
     type = models.ForeignKey(
         DescriptionType, on_delete=models.PROTECT, blank=False, null=False
